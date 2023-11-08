7e3b17e16c7712fffb78ca30cb46ebb861dcb22c
Lazaro-Costa <lazarocosta.dev@gmail.com>
2023-11-08 08:48:25 -0300

    Feat: add Swagger API Documentation

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ description: 'Título do Post' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ description: 'Conteúdo do Post' })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiProperty({ description: 'Email do Autor' })
   @IsEmail()
   authorEmail: string;
 }
